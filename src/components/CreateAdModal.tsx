3f85df5c2c80caa41527bbe2ff1d5e9ddde8e171
Kaique Campos <kaiquecamposferreira@gmail.com>
2022-09-21 16:42:20 -0300

    commit before adding responsiveness

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -60,8 +60,8 @@ export function CreateAdModal() {
             <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
             <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-                <Dialog.Title className="text-3xl front-black">
-                    Públique um anúncio
+                <Dialog.Title className="text-3xl font-black">
+                    Publique um anúncio
                 </Dialog.Title>
 
                 <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
@@ -113,7 +113,7 @@ export function CreateAdModal() {
 
                     <div className="flex gap-6">
                         <div className="flex flex-col gap-2">
-                            <label htmlFor="weekDays">JQuando costuma jogar?</label>
+                            <label htmlFor="weekDays">Quando costuma jogar?</label>
 
                             <ToggleGroup.Root
                                 type="multiple"
